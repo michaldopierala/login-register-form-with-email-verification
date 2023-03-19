cca890c03f1cc82b3a65d5ade17c02e2c391c4e6
michaldopierala <michal6452@gmail.com>
2023-03-19 11:07:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login Form </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="Login Form" />
+    <!-- <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+
+    <!-- <script src="https://kit.fontawesome.com/af562a2a63.js" crossorigin="anonymous"></script> -->
+
+</head>
+<body>
+    
+</body>
+</html>
